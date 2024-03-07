debd91e302f30864fa9932a51a122f5923a8e97b
a0988306629 <a0988306629@gmail.com>
2024-03-08 01:40:59 +0800

    socket

--- pythonforESP32.py
+++ pythonforESP32.py
@@ -1,33 +1,27 @@
-import sys 
-import network
+import socket
+import threading
 
-def WiFi_connect(ssid_c, password_c) :
-    wlan = network.WLAN(network,STA_IF)
-    wlan.active(True)
-    
-    if wlan.isconnected() :
-        print("ESP32 is connected!")
-    print("Searching WiFi")
-    ssid_all = wlan.scan()
-    
-    for ssid in ssid_all : 
-        if ssid_c == bytes.decode(ssid[0]) :
-            print("find ESP32")
-            break
-        else :
-            print("Cant find ESP32")
-            sys.exit(-1)
-            return 0
-    try :
-        wlan.connect(ssid_c,password)
-        print("connecting")
-        delay(2500)
-        while not wlan.isconnected() :
-            pass
-    except :
-        print("ERROR")
-        sys.exit(-1)
-    return wlan
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setblocking(False)
 
-if __name__ == "__main__" :
-    wlan = WiFi_connect("ESP32_Master","3B017126")
+try :
+    s.connect(('192.168.4.1', 80))
+except:
+    print("ERROR!")
+    exit()
+
+def sendMes() :
+    while(true):
+        s.sendall(input())
+
+def revMes() :
+    while(true) :
+        data = s.recv(1024)
+        if data!='' :
+            print("recv : "+ data)
+
+t1 = threading.Thread(target=sendMes)
+t1.start()
+
+t2 = threading.Thread(target=revMes)
+t2.start() 
